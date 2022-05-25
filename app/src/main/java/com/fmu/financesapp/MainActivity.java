746475ffbfcd70af4e9f47a368ec3ecd1b2fee15
Joao Souza <joao.souza@avenuecode.com>
2022-05-24 21:57:23 -0300

    Configured recycle view to receive data

--- app/src/main/java/com/fmu/financesapp/MainActivity.java
+++ app/src/main/java/com/fmu/financesapp/MainActivity.java
@@ -1,21 +1,21 @@
 package com.fmu.financesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.fmu.financesapp.dao.AccountDao;
 import com.fmu.financesapp.databinding.ActivityMainBinding;
+import com.fmu.financesapp.fragments.HomeFragment;
+import com.fmu.financesapp.model.Account;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-
+    private ActivityMainBinding binding ;
+    private final AccountDao accountList = new AccountDao();
     private NavHostFragment navHostFragment;
     private NavController navController;
 
@@ -25,14 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        setMockData();
         initNavigation();
+        initHomeRecyle(savedInstanceState);
+    }
+
+    private void initHomeRecyle(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.srlRecentActivity, HomeFragment.class, null)
+                    .commit();
+        }
     }
 
     private void initNavigation() {
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
         navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+    }
+
+    private void setMockData(){
+        accountList.save(new Account("teste", 1000.0, "Mercado Açai", false));
+        accountList.save(new Account("teste", 1000.0, "Vendas", true));
+        accountList.save(new Account("teste", 1000.0, "Contas", false));
+        accountList.save(new Account("teste", 1000.0, "Lanche", false));
+        accountList.save(new Account("teste", 1000.0, "Comissão", true));
     }
 }
 
