3ad2e974b925e393bc13bfd5f3fe31785a169c64
Cristyane <christiehespanha@gmail.com>
2022-06-13 01:37:05 -0300

    made floating action button expandable

--- app/src/main/java/com/fmu/financesapp/MainActivity.java
+++ app/src/main/java/com/fmu/financesapp/MainActivity.java
@@ -1,23 +1,31 @@
 package com.fmu.financesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-
-import android.os.Bundle;
 
 import com.fmu.financesapp.dao.AccountDao;
 import com.fmu.financesapp.databinding.ActivityMainBinding;
 import com.fmu.financesapp.fragments.HomeFragment;
 import com.fmu.financesapp.model.Account;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
     private final AccountDao accountList = new AccountDao();
-    private NavHostFragment navHostFragment;
-    private NavController navController;
+
+    private FloatingActionButton fabTransactions;
+    private FloatingActionButton fabGoals;
+    private TextView tvTransactions, tvGoals;
+    private View overlay;
+
+    private Boolean isVisible = false;
 
 
     @Override
@@ -29,6 +37,38 @@ public class MainActivity extends AppCompatActivity {
         initNavigation();
         initHomeRecycle(savedInstanceState);
 
+        FloatingActionButton fabMain = binding.fabExpand;
+        fabTransactions = binding.fabTransaction;
+        fabGoals = binding.fabGoal;
+        tvTransactions = binding.tvFabTransaction;
+        tvGoals = binding.tvFabGoal;
+        overlay = binding.viewOverlay;
+
+        fabMain.setOnClickListener(view -> {
+            if (!isVisible) {
+                showOverlayView();
+                fabTransactions.show();
+                tvTransactions.setVisibility(View.VISIBLE);
+                fabGoals.show();
+                tvGoals.setVisibility(View.VISIBLE);
+            } else {
+                hideOverlayView();
+                fabTransactions.hide();
+                tvTransactions.setVisibility(View.GONE);
+                fabGoals.hide();
+                tvGoals.setVisibility(View.GONE);
+            }
+            isVisible = !isVisible;
+        });
+
+    }
+
+    private void showOverlayView() {
+        overlay.animate().alpha(0.7f);
+    }
+
+    private void hideOverlayView() {
+        overlay.animate().alpha(0f);
     }
 
     private void initHomeRecycle(Bundle savedInstanceState) {
@@ -37,22 +77,22 @@ public class MainActivity extends AppCompatActivity {
                     .setReorderingAllowed(true)
                     .add(R.id.srlRecentActivity, HomeFragment.class, null)
                     .commit();
-
         }
     }
 
     private void initNavigation() {
-        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
-        navController = navHostFragment.getNavController();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
     }
 
-    private void setMockData(){
+    private void setMockData() {
         accountList.save(new Account("teste", 1000.0, "Mercado Açai", false));
         accountList.save(new Account("teste", 1000.0, "Vendas", true));
         accountList.save(new Account("teste", 1000.0, "Contas", false));
         accountList.save(new Account("teste", 1000.0, "Lanche", false));
-        accountList.save(new Account("teste", 1000.0, "Comissão", true));
+        accountList.save(new Account("teste", 1000.0, "Comes", true));
     }
 }
 
