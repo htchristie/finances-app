8753b15b00bff5de90bbbc57fb28e50f7e644ab1
Joao Souza <joao.souza@avenuecode.com>
2022-06-13 11:38:46 -0300

    Add some mocks

--- app/src/main/java/com/fmu/financesapp/MainActivity.java
+++ app/src/main/java/com/fmu/financesapp/MainActivity.java
@@ -9,16 +9,18 @@ import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
 
 import com.fmu.financesapp.dao.AccountDao;
+import com.fmu.financesapp.dao.CategoryDao;
 import com.fmu.financesapp.databinding.ActivityMainBinding;
 import com.fmu.financesapp.fragments.HomeFragment;
 import com.fmu.financesapp.model.Account;
+import com.fmu.financesapp.model.Category;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private final AccountDao accountList = new AccountDao();
+    private final CategoryDao category = new CategoryDao();
     private NavHostFragment navHostFragment;
     private NavController navController;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setMockData();
+        setCategorieData();
         initNavigation();
         initHomeRecycle(savedInstanceState);
 
@@ -48,11 +51,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setMockData(){
-        accountList.save(new Account("teste", 1000.0, "Mercado Açai", false));
-        accountList.save(new Account("teste", 1000.0, "Vendas", true));
+        accountList.save(new Account("teste", 1000.0, "Contas", false));
+        accountList.save(new Account("teste", 1000.0, "Contas", true));
         accountList.save(new Account("teste", 1000.0, "Contas", false));
         accountList.save(new Account("teste", 1000.0, "Lanche", false));
-        accountList.save(new Account("teste", 1000.0, "Comissão", true));
+        accountList.save(new Account("teste", 1000.0, "Lanche", true));
+    }
+
+    private void setCategorieData(){
+        category.save(new Category("Contas", 1000.0, "ivPlCategoryIcon"));
+        category.save(new Category("Lanche", 1000.0, "ivPlCategoryIcon"));
     }
 }
 
