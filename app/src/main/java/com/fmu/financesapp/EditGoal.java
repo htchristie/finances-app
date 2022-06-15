7c308ba9546f60f7ad16395ae4413ac8f4cdbdcf
Cristyane <christiehespanha@gmail.com>
2022-06-15 15:09:34 -0300

    added missing methods

--- app/src/main/java/com/fmu/financesapp/EditGoal.java
+++ app/src/main/java/com/fmu/financesapp/EditGoal.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.fmu.financesapp.databinding.ActivityAddTransactionBinding;
 import com.fmu.financesapp.databinding.ActivityEditGoalBinding;
@@ -12,6 +16,14 @@ import com.fmu.financesapp.databinding.ActivityEditGoalBinding;
 public class EditGoal extends AppCompatActivity {
 
     private ActivityEditGoalBinding binding;
+
+    protected void onResume() {
+        super.onResume();
+        String[] categories = getResources().getStringArray(R.array.categories_expense);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.transactions_dropdown_item, categories);
+        AutoCompleteTextView autoCompleteTextView = binding.tvGoalCategory;
+        autoCompleteTextView.setAdapter(adapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +38,12 @@ public class EditGoal extends AppCompatActivity {
         if (editGoalEditBar != null) {
             editGoalEditBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.edit_goal_menu, menu);
+        return true;
     }
 }
