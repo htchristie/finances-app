18875d6cba515783e239d588448aadf60ff41f1a
Cristyane <christiehespanha@gmail.com>
2022-06-16 14:17:18 -0300

    added datepicker

--- app/src/main/java/com/fmu/financesapp/AddTransaction.java
+++ app/src/main/java/com/fmu/financesapp/AddTransaction.java
@@ -4,17 +4,28 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 
 import com.fmu.financesapp.databinding.ActivityAddTransactionBinding;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
 
 public class AddTransaction extends AppCompatActivity {
 
     private ActivityAddTransactionBinding binding;
+
+    TextInputEditText tvTransactionDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onResume() {
@@ -26,6 +37,28 @@ public class AddTransaction extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityAddTransactionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        tvTransactionDate = binding.tvTransactionDate;
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvTransactionDate.setOnClickListener(view -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    AddTransaction.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    setListener, year, month, day);
+            datePickerDialog.getDatePicker().setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
+            datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            datePickerDialog.show();
+        });
+
+        setListener = (datePicker, year1, month1, day1) -> {
+            month1 = month1 +1;
+            String date = day1 +"/"+ month1 +"/"+ year1;
+            tvTransactionDate.setText(date);
+        };
 
         Toolbar addTransactionToolbar = binding.toolbarTransactions;
         setSupportActionBar(addTransactionToolbar);
--- app/src/main/java/com/fmu/financesapp/EditTransaction.java
+++ app/src/main/java/com/fmu/financesapp/EditTransaction.java
@@ -4,26 +4,57 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 
 import com.fmu.financesapp.databinding.ActivityEditGoalBinding;
 import com.fmu.financesapp.databinding.ActivityEditTransactionBinding;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
 
 public class EditTransaction extends AppCompatActivity {
 
     private ActivityEditTransactionBinding binding;
+
+    TextInputEditText tvTransactionDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityEditTransactionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        tvTransactionDate = binding.tvTransactionDate;
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvTransactionDate.setOnClickListener(view -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    EditTransaction.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    setListener, year, month, day);
+            datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            datePickerDialog.show();
+        });
+
+        setListener = (datePicker, year1, month1, day1) -> {
+            month1 = month1 +1;
+            String date = day1 +"/"+ month1 +"/"+ year1;
+            tvTransactionDate.setText(date);
+        };
 
         Toolbar editTransactionToolbar = binding.toolbarEditTransaction;
         setSupportActionBar(editTransactionToolbar);
