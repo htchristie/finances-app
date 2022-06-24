9941886fa1491750d21a9680a62f6857350125c7
Cristyane <christiehespanha@gmail.com>
2022-06-24 06:24:00 -0300

    added scrollview to 'activity add transaction'

--- app/src/main/java/com/fmu/financesapp/fragments/StatisticsFragment.java
+++ app/src/main/java/com/fmu/financesapp/fragments/StatisticsFragment.java
@@ -14,9 +14,6 @@ import com.fmu.financesapp.databinding.FragmentStatisticsBinding;
 
 public class StatisticsFragment extends Fragment {
 
-    private FragmentStatisticsBinding binding;
-    private TextView tvFood, tvEducation, tvMarket, tvHealth, tvTransport, tvOther;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +23,6 @@ public class StatisticsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
-        setPercentages();
-        return view;
-    }
-
-    private void setPercentages() {
-        tvFood = binding.tvStFoodPercentage;
-        tvEducation = binding.tvStEducationPercentage;
-        tvHealth = binding.tvStHealthPercentage;
-        tvMarket = binding.tvStMarketPercentage;
-        tvTransport = binding.tvStTransportPercentage;
-        tvOther = binding.tvStOtherPercentage;
-
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
     }
 }
